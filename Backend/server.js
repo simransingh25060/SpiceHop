31ba4639d7f4dc2ee3d336f0ef5256bc1ba0e715
Simran Singh <simransingh25060@gmail.com>
2025-11-03 01:00:53 +0530

    connect to MongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,11 @@
+require('dotenv').config(); 
 const app = require('./src/app');
+const connectDB = require('./src/db/db');
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
---
+++ Backend/src/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+function connectDB() {
+    mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+    })
+}
+
+module.exports = connectDB;
