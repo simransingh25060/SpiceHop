f48c5d9b91c5a01441cd66e3ed1db09b38dc5736
Simran Singh <simransingh25060@gmail.com>
2025-11-05 23:56:21 +0530

    Feat: add food partner auth (model & controller)

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const userModel = require('../models/user.model');
+const foodPartnerModel = require("../models/foodpartner.model")
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
@@ -41,12 +42,152 @@ res.status(201).json({
 })
 }
 
-async function loginUser(req, res) { }
+async function loginUser(req, res) { 
+    const {email, password} = req.body;
+
+    const user = await userModel.findOne({
+        email
+    })
+
+    if(!user) {
+        return res.status(404).json({   
+            message: "Invalid email or password"
+})
+}
+
+const isPasswordValid = await bcrypt.compare(password, user.password);
+
+if(!isPasswordValid) {
+    return res.status(404).json({
+        message: "Invalid email or password"
+    })  
+
+}
+
+const token = jwt.sign({
+    id: user._id,   
+    }, process.env.JWT_SECRET)
+
+res.cookie("token", token)
+
+res.status(200).json({
+    message: "User logged in successfully",
+    user: {
+        _id: user._id,
+        email: user.email,
+        fullName: user.fullName
+    }
+})
+
+
+}
+
+function logoutUser(req, res) {
+    res.clearCookie("token");
+    res.status(200).json({
+        message: "User logged out successfully"
+    });
+
+}
+
+
+async function registerFoodPartner(req, res) {
+
+    const {name, email, password} = req.body;
+
+    const isAccountAlreadyExists = await foodPartnerModel.findOne({
+        email
+    })
+
+    if (isAccountAlreadyExists) {
+        return res.status(400).json({
+            message: "Food partner account already exists"
+        })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const foodPartner = await foodPartnerModel.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    const token = jwt.sign({
+        id: foodPartner._id,
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+    res.status(201).json({
+        message: "Food partner registered successfully",
+        foodPartner: {
+            _id: foodPartner._id,
+            email: foodPartner.email,
+            name: foodPartner.name
+        }
+    })
+    
+
+}
+
+async function loginFoodPartner(req, res) {
+
+    const {email, password} = req.body; 
+
+    const foodPartner = await foodPartnerModel.findOne({
+        email
+    })
+
+    if(!foodPartner) {
+        return res.status(400).json({
+            message: "Invalid email or password"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, foodPartner.password);
+
+    if(!isPasswordValid) {
+        return res.status(400).json({
+            message: "Invalid email or password"
+        })
+    }
+
+    const token = jwt.sign({
+        id: foodPartner._id,
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+    res.status(201).json({
+        message: "Food partner logged in successfully",
+        foodPartner: {
+            _id: foodPartner._id,
+            email: foodPartner.email,
+            name: foodPartner.name
+        }
+    })
+
+}
+
+function logoutFoodPartner(req, res) {
+    res.clearCookie("token");
+    res.status(200).json({
+        message: "Food partner logged out successfully"
+    });
+
+}
 
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser,
+    registerFoodPartner,
+    loginFoodPartner,
+    logoutFoodPartner
 }
+
+
 
 //password- hashing => data in database is secure
 //password- not sent to frontend
---
+++ Backend/src/models/foodpartner.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const foodPartnerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+
+}, 
+{timestamps: true})
+
+const foodPartnerModel = mongoose.model("foodpartner", foodPartnerSchema);
+
+module.exports = foodPartnerModel;
+
