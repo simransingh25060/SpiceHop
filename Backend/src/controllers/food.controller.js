5cfb0fdc92e3118d67395144a6cbb67851c2ea6b
Simran Singh <simransingh25060@gmail.com>
2025-11-11 11:44:50 +0530

    feat: add food creation controller and image upload service

--- Backend/src/controllers/food.controller.js
+++ Backend/src/controllers/food.controller.js
@@ -1,4 +1,6 @@
 const foodModel = require('../models/food.model');
+const storageService = require('../services/storage.service');
+const { v4: uuid } = require("uuid");
 
 async function createFood(req, res) {
     console.log(req.foodPartner)
@@ -6,7 +8,19 @@ async function createFood(req, res) {
     console.log(req.body)
     console.log(req.file)
 
-    res.send("Food item created")
+    const fileUploadResult = await storageService.uploadFile(req.file.buffer, uuid())
+
+    const foodItem = await foodModel.create({
+        name: req.body.name,
+        description: req.body.description,
+        video: fileUploadResult.url,
+        foodPartner: req.foodPartner._id
+    })
+
+    res.status(201).json({
+        message: "food created successfully",
+        food: foodItem
+    })
 }
 
 module.exports = {
